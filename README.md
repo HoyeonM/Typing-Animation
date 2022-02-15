65a42e5360ad405c738d53075899eb0d4aa24366
Hoyeon Moon <mhy991011@gmail.com>
2022-02-14 23:43:00 -0800

    added readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Study log 2: Typing Animation
+#### I used gsap animation library to create typing animation. 
+I learned how gsap.to(), gsap.from() and timeline() works. 
+##### More comments you can see in app.js !
 
+
+![](typeanimation.gif)
+
+##### sourch from: <https://youtu.be/ZT66N5hBiCE>
--- app.js
+++ app.js
@@ -10,14 +10,17 @@ boxTL.to('.box', {duration: 1, width:"25vw",
 delay: 0.5, ease: "power4.easeIn"}) //box goes to 25 vw for 1 second
  .from('.hi', {duration: 1, y:"8.5vw", ease: "power3.out", onComplete: () => masterTL.play()})
  //y:start lower
+ //from: define where the values should START,it animates to the current state
  .to('.box', {duration: 1, height:"8.5vw", ease: "elastic.out"}) //box fill out the '.hi'
  .to('.box', {duration: 2, autoAlpha: 0.5, yoyo: true, repeat: -1,})
+ //to: define the destination values
  //autoAlpha controls opacity
 
 let masterTL = gsap.timeline({repeat: -1}).pause()//want to pause it before '.hi' is done
 
 words.forEach(word => {
     let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
+    //yoyo makes text looks like it's deleting(backspace)
     tl.to('.text', {duration: 1, text: word})
-    masterTL.add(tl)
+    masterTL.add(tl) //make words coming out infinitely
 })
